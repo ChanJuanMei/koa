b50e44d09705e1748affbccce944f409e70ca298
明月 <chanjuan.mei@melot.cn>
2021-02-01 18:05:58 +0800

    test

--- src/web/layout/MainLayout.jsx
+++ src/web/layout/MainLayout.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
-import { Layout, Menu, Icon, Breadcrumb } from 'antd'
+import { Layout, Menu, Breadcrumb } from 'antd'
 const { Header, Content, Sider } = Layout;
-
 const { SubMenu } = Menu;
+import 'antd/dist/antd.css';
 
 
 class MainLayout extends Component {
@@ -21,7 +21,7 @@ class MainLayout extends Component {
     return (
       <div>
       {/* layout */}
-         {/* <Layout>
+         <Layout>
           <Header className="header">
             <div className="logo" />
             <Menu
@@ -43,19 +43,19 @@ class MainLayout extends Component {
                 defaultOpenKeys={['sub1']}
                 style={{ height: '100%', borderRight: 0 }}
               >
-                <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
+                <SubMenu key="sub1" title={<span>subnav 1</span>}>
                   <Menu.Item key="1">option1</Menu.Item>
                   <Menu.Item key="2">option2</Menu.Item>
                   <Menu.Item key="3">option3</Menu.Item>
                   <Menu.Item key="4">option4</Menu.Item>
                 </SubMenu>
-                <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
+                <SubMenu key="sub2" title={<span>subnav 2</span>}>
                   <Menu.Item key="5">option5</Menu.Item>
                   <Menu.Item key="6">option6</Menu.Item>
                   <Menu.Item key="7">option7</Menu.Item>
                   <Menu.Item key="8">option8</Menu.Item>
                 </SubMenu>
-                <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
+                <SubMenu key="sub3" title={<span>subnav 3</span>}>
                   <Menu.Item key="9">option9</Menu.Item>
                   <Menu.Item key="10">option10</Menu.Item>
                   <Menu.Item key="11">option11</Menu.Item>
@@ -73,11 +73,11 @@ class MainLayout extends Component {
                 background: '#fff', padding: 24, margin: 0, minHeight: 280,
               }}
               >
-                Content
+                App~~~~
               </Content>
             </Layout>
           </Layout>
-        </Layout> */}
+        </Layout>
         {/* layout */}
      </div>
     )
